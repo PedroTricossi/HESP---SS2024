6d31031171c4f8aaf243f06131a1ba65c65aba4d
Pedro Tricossi <53714676+PedroTricossi@users.noreply.github.com>
2024-04-29 21:21:09 +0200

    FIX: complex problems

--- task2/Julia.cu
+++ task2/Julia.cu
@@ -2,27 +2,29 @@
 #include "device_launch_parameters.h"
 #include <math.h>
 #include <iostream>
-#include "CImg.h"
-#include <complex>
+#include <CImg.h>
+#include <cuda/std/complex>
 
 using namespace cimg_library;
+using complex_t = cuda::std::complex<double>;
 
 const int WIDTH = 800;
 const int HEIGHT = 800;
 const int MAX_ITER = 128;
 const double THRESHOLD = 10.0;
-const std::complex<double> Julia_C(-0.8, 0.2);
+
 
 // CUDA kernel to compute Julia set
 __global__ void computeJuliaSet(unsigned char* imageData) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
+    complex_t Julia_C(-0.8, 0.156); // Julia set constant
 
     if (x < WIDTH && y < HEIGHT) {
         double real = (double)x / WIDTH * 4 - 2;
         double imag = (double)y / HEIGHT * 4 - 2;
-        std::complex<double> z0(real, imag);
-        std::complex<double> z = z0;
+        cuda::std::complex<double> z0(real, imag);
+        cuda::std::complex<double> z = z0;
          int iterations = 0;
         while (abs(z) <= THRESHOLD && iterations < MAX_ITER) {
             z = z * z + Julia_C;

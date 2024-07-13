b31b53a70170c7f67d0a3ad8a5813561f80b2950
BallasticMonkey <62638445+BallasticMonkey@users.noreply.github.com>
2024-07-13 12:38:40 +0200

    Update particle_simulation.cu

--- task6/particle_simulation.cu
+++ task6/particle_simulation.cu
@@ -152,7 +152,8 @@ void start_particle_simulation(int time_steps, float step_size, int num_particle
 }
 
 /*particle simulation using graphs
-void start_particle_simulation(int time_steps, float step_size, int num_particles, float eps, float sigma, float k_n, float gamma, float gravity, float box_extension, float cut_off_radious) {
+void start_particle_simulation(int time_steps, float step_size, int num_particles, float eps, float sigma, float k_n, float gamma, float gravity, float box_extension, float cut_off_radious)
+{
     float3 *ha_forces;
     float3 *hb_forces;
     float3 *da_forces;
@@ -178,8 +179,8 @@ void start_particle_simulation(int time_steps, float step_size, int num_particle
     cudaMallocHost((void**)&ha_forces, sizeof(float3) * (num_particles / 2));
     cudaMallocHost((void**)&hb_forces, sizeof(float3) * (num_particles / 2));
 
-    Particle3D* d_a = NULL;
-    Particle3D* d_b = NULL;
+    Particle3D* d_a = NULL; 
+    Particle3D* d_b = NULL; 
 
     cudaMalloc((void**)&d_a, sizeof(Particle3D) * (num_particles / 2));
     cudaMalloc((void**)&d_b, sizeof(Particle3D) * (num_particles / 2));
@@ -187,6 +188,9 @@ void start_particle_simulation(int time_steps, float step_size, int num_particle
     cudaMalloc((void**)&db_forces, sizeof(float3) * (num_particles / 2));
 
     cudaStream_t stream[2];
+    cudaGraph_t graph[2];
+    cudaGraphExec_t instance[2];
+
     for (int i = 0; i < 2; i++) {
         cudaStreamCreate(&stream[i]);
     }
@@ -196,95 +200,79 @@ void start_particle_simulation(int time_steps, float step_size, int num_particle
     int pos = 0;    
 
     for (int i = 0; i < num_particles; ++i) {
-        float x = fmod(pos, box_extension);
-        float y = (pos >= box_extension) ? fmod(floor(pos * 2 / box_extension), box_extension) : 0;
-        float z = (pos * 4 >= box_extension * box_extension) ? fmod(floor((pos * 4) / (box_extension * box_extension)), box_extension) : 0;
-        
-        if(i < (num_particles / 2))
-            h_a[i] = Particle3D(float3{ x, y, z }, float3{ 0.0f, 0.0f, 0.0f }, 1.0f, 1.0f, nullptr, i);
-        else
-            h_b[i - (num_particles / 2)] = Particle3D(float3{ x, y, z }, float3{ 0.0f, 0.0f, 0.0f }, 1.0f, 1.0f, nullptr, i);
-        
-        if (i < num_particles / 2)
-            ha_forces[i] = float3{ 0.0f, 0.0f, 0.0f };
-        else
-            hb_forces[i - (num_particles / 2)] = float3{ 0.0f, 0.0f, 0.0f };
+        float x = fmod(pos, box_extension) ;
+        float y = (pos >= box_extension) ? fmod(floor(pos * 2 / box_extension), box_extension): 0;
+        float z = (pos * 4 >= box_extension * box_extension) ? fmod(floor((pos * 4) / (box_extension * box_extension) ), box_extension) : 0;
 
+        if (i < (num_particles / 2)) {
+            h_a[i] = Particle3D(float3{ x, y, z }, float3{ 0.0f, 0.0f, 0.0f }, 1.0f, 1.0f, nullptr, i);
+            ha_forces[i] = float3{ 0.0f, 0.0f, 0.0f };
+        } else {
+            h_b[i - (num_particles / 2)] = Particle3D(float3{ x, y, z }, float3{ 0.0f, 0.0f, 0.0f }, 1.0f, 1.0f, nullptr, i);
+            hb_forces[i - (num_particles / 2)] = float3{ 0.0f, 0.0f, 0.0f };
+        }
+        
         pos += 2;
     }
 
+    std::cout << num_particles << ", ";
+
     cudaMemcpyAsync(d_a, h_a, sizeof(Particle3D) * (num_particles / 2), cudaMemcpyHostToDevice, stream[0]);
     cudaMemcpyAsync(d_b, h_b, sizeof(Particle3D) * (num_particles / 2), cudaMemcpyHostToDevice, stream[1]);
+
     cudaMemcpyAsync(da_forces, ha_forces, sizeof(float3) * (num_particles / 2), cudaMemcpyHostToDevice, stream[0]);
     cudaMemcpyAsync(db_forces, hb_forces, sizeof(float3) * (num_particles / 2), cudaMemcpyHostToDevice, stream[1]);
 
-    cudaGraph_t graph[2];
-    cudaGraphExec_t instance[2];
-
-    // Create and capture graphs for both streams
-    for (int i = 0; i < 2; i++) {
-        cudaStreamBeginCapture(stream[i], cudaStreamCaptureModeGlobal);
-
-        // Reset forces
-        cudaMemset(i == 0 ? da_forces : db_forces, 0, sizeof(float3) * (num_particles / 2));
-
-        // Compute forces using CUDA
-        compute_force_between_particles<<<numberOfBlocks, numberOfThreads, 0, stream[i]>>>(
-            i == 0 ? d_a : d_b,
-            i == 0 ? da_forces : db_forces,
-            num_particles / 2,
-            eps,
-            sigma,
-            k_n,
-            gamma,
-            gravity,
-            box_extension,
-            cut_off_radious,
-            nb_list
-        );
-
-        // Integrate particles using CUDA
-        apply_integrator_for_particle_euler<<<numberOfBlocks, numberOfThreads, 0, stream[i]>>>(
-            i == 0 ? d_a : d_b,
-            i == 0 ? da_forces : db_forces,
-            num_particles / 2,
-            step_size,
-            box_extension
-        );
-
-        cudaStreamEndCapture(stream[i], &graph[i]);
-        cudaGraphInstantiate(&instance[i], graph[i], NULL, NULL, 0);
-    }
-
     for (int step = 0; step < time_steps; ++step) {
+        cudaMemset(da_forces, 0, sizeof(float3) * (num_particles / 2));
+        cudaMemset(db_forces, 0, sizeof(float3) * (num_particles / 2));
+
+        // Begin capture for stream 0
+        cudaStreamBeginCapture(stream[0], cudaStreamCaptureModeGlobal);
+
+        compute_force_between_particles <<< numberOfBlocks, numberOfThreads, 0, stream[0]>>> (d_a, da_forces, num_particles, eps, sigma, k_n, gamma, gravity, box_extension, cut_off_radious, nb_list);
+        apply_integrator_for_particle_euler <<< numberOfBlocks, numberOfThreads, 0, stream[0]>>> (d_a, da_forces, num_particles, step_size, box_extension);
+
+        // End capture for stream 0
+        cudaStreamEndCapture(stream[0], &graph[0]);
+        cudaGraphInstantiate(&instance[0], graph[0], NULL, NULL, 0);
+
+        // Begin capture for stream 1
+        cudaStreamBeginCapture(stream[1], cudaStreamCaptureModeGlobal);
+
+        compute_force_between_particles <<< numberOfBlocks, numberOfThreads, 0, stream[1]>>> (d_b, db_forces, num_particles, eps, sigma, k_n, gamma, gravity, box_extension, cut_off_radious, nb_list);
+        apply_integrator_for_particle_euler <<< numberOfBlocks, numberOfThreads, 0, stream[1]>>> (d_b, db_forces, num_particles, step_size, box_extension);
+
+        // End capture for stream 1
+        cudaStreamEndCapture(stream[1], &graph[1]);
+        cudaGraphInstantiate(&instance[1], graph[1], NULL, NULL, 0);
+
         for (int i = 0; i < 2; i++) {
             cudaGraphLaunch(instance[i], stream[i]);
         }
 
-        // Synchronize streams
+        cudaDeviceSynchronize();
+
+        // Cleanup the graph instances after each iteration
         for (int i = 0; i < 2; i++) {
-            cudaStreamSynchronize(stream[i]);
+            cudaGraphDestroy(graph[i]);
+            cudaGraphExecDestroy(instance[i]);
         }
-
-        // Optional: Write the VTK file
-        // writeVTKFile(step + 1, num_particles, particles);
-    }
-
-    // Cleanup
-    for (int i = 0; i < 2; i++) {
-        cudaGraphDestroy(graph[i]);
-        cudaGraphExecDestroy(instance[i]);
-        cudaStreamDestroy(stream[i]);
     }
 
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(da_forces);
     cudaFree(db_forces);
+
     cudaFreeHost(h_a);
     cudaFreeHost(h_b);
     cudaFreeHost(ha_forces);
     cudaFreeHost(hb_forces);
+
+    for (int i = 0; i < 2; i++) {
+        cudaStreamDestroy(stream[i]);
+    }
 }
 
 

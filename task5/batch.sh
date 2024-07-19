7ee0249afabd3965059bd6e430fa58665020e71c
Pedro Domingos Tricossi dos Santos <hesp123h@alex2.nhr.fau.de>
2024-07-20 00:23:32 +0200

    sync

--- task5/batch.sh
+++ task5/batch.sh
@@ -4,8 +4,9 @@
 #SBATCH --job-name=particle_simulation
 #SBATCH --export=NONE
 #SBATCH --output=R-%x.%j.out
-#SBATCH --error=R-%x.%j.err
 
 unset SLURM_EXPORT_ENV
-module load load nvhpc cuda
-./run 10 0.1 262144 1 1 300 2.5 2 6.4 1 
+module load nvhpc cuda
+
+#nsys profile --stats=true -o particle-report ./run 100 0.1 32768 1 1 300 2.5 2 6.4 1 
+./run 10 0.1 262144 1 1 300 2.5 6 11 1
